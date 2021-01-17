5b9f4485bc9d63b00dd54ca0cd4561ac6a5d447e
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2021-01-17 01:29:52 -0600

    Added field

---
+++ example/unity/Demo/Assets/TrashController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashController : MonoBehaviour
+{
+    public Object[] small_objects;
+    public Object[] big_objects;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0, 20 * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+    }
+}
